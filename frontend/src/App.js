e22b03a8ac3db1622b63af3001b167de1a0242d8
sayajirao <sayajirao1998@gmail.com>
2026-04-10 10:03:23 +0530

    did chnges in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>Happy Birthday to sheebaaa {new Date().toLocaleTimeString()}</h1>
+                    <h1>Happy Birthday to Sayaaaa {new Date().toLocaleTimeString()}</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
